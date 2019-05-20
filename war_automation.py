5bf48dc78ae16e94e3cbf6441704609310676555
Aram <50476431+aramupwork@users.noreply.github.com>
2019-05-20 19:57:00 +0400

    Automate questionnaire filling
    - All questions based on the input file are filled
    - Minor changes in the input file format

--- war_automation.py
+++ war_automation.py
@@ -61,11 +61,11 @@ def run_command(command_str):
         exit_code = process.wait()
         if args.debug:
             logging.info(stdout)
-            print(stdout)
+            print(stdout.decode())
     except Exception as err:
         if args.debug:
             logging.exception(err)
-            print(str(err))
+        print(str(err))
     finally:
         return exit_code, stdout, stderr
 
@@ -124,20 +124,25 @@ def get_input_data(input_file_path):
     # Specify default values for some parameters for the case they are missing in the input file.
     # Read, validate and return the input file (ini) content.
     default_values = {'signin.url': 'https://console.aws.amazon.com'}
-    configs = configparser.ConfigParser()
     try:
-        configs.read(input_file_path)
-        configs['DEFAULT'] = default_values
-        war_mandatory_keys = ['name', 'description', 'industryType', 'industry', 'environment', 'regions', 'accountIDs']
+        if 3 <= get_python_version():
+            configs = configparser.ConfigParser()
+            configs.read(input_file_path)
+            configs['DEFAULT'] = default_values
+        else:
+            configs = configparser.ConfigParser(default_values)
+            configs.read(input_file_path)
+        war_mandatory_keys = ['name', 'description', 'industryType', 'industry', 'environment', 
+                              'regions', 'accountIDs']
         for key in war_mandatory_keys:
             if not configs.has_option('WAR', key):
                 print('Key \'' + key + '\' is missing in the input file.')
                 exit(4)
         return configs
     except configparser.Error as err:
-        print(str(err))
         if args.debug:
             logging.exception(err)
+        print(str(err))
         exit(4)
 
 def request_data(prompt, input_mask = True):
@@ -163,20 +168,20 @@ def open_browser():
     try:
         driver = webdriver.Chrome(options = chrome_options)
     except Exception as err:
+        print(str(err))
         if args.debug:
             logging.exception(err)
-        print(str(err))
         exit(3)
     browser_version = driver.capabilities['version']
     if args.debug:
         info_str = 'Chrome Browser Version: ' + browser_version
-        logging.info(info_str)
         print(info_str)
+        logging.info(info_str)
     driver_version = driver.capabilities['chrome']['chromedriverVersion'].split()[0]
     if args.debug:
         info_str = 'Chrome Driver Version: ' + driver_version
-        logging.info(info_str)
         print(info_str)
+        logging.info(info_str)
     return driver
 
 def open_url(driver, configs):
@@ -203,9 +208,16 @@ def get_element(driver, locator, by_state, max_wait = 20):
     except Exception as err:
         if args.debug:
             logging.exception(err)
-        print('The element \'' + locator[1] + '\' is not found or it is in inaccessable state.\nCheck log for more info.')
+        print('The element \'' + locator[1] + '\' is not found or it is in inaccessible state.\n' + \
+              'Check log for more info.')
         exit(6)
     return button
+
+def get_elements(driver, locator, by_state, max_wait = 20):
+    elements = WebDriverWait(driver, max_wait).until(
+                             EC.presence_of_all_elements_located(locator))
+    return elements
+    #visibility_of_all_elements_located
 
 def enter_string_with_delay(field, str_to_type):
     if args.debug:
@@ -320,7 +332,7 @@ def create_workload(driver, configs):
     actions.perform()
 
     aws_regions_checkbox = get_element(driver, (By.XPATH, '//*[@id="workloadRegionsCheckbox"]//input[@type="checkbox"]'), 
-                                       'presence')
+                                                'presence')
     aws_regions_checkbox.click()
     regions = configs.get('WAR', 'regions').lower().split(',')
     for region in regions:
@@ -347,10 +359,121 @@ def create_workload(driver, configs):
     if not skip_ids:
         account_ids_textarea = get_element(driver, (By.ID, 'awsui-textarea-2'), 'clickable')
         account_ids_textarea.send_keys(account_ids)
-    # TODO remove time.sleep call below
-    time.sleep(30)
     define_workload_button = get_element(driver, (By.ID, 'defineWorkload-createWorkloadButton'), 'clickable')
     define_workload_button.click()
+
+def check_loading_state(driver, question_text):
+    # Wait until the next question will be loaded
+    try:
+        max_wait = 60
+        while question_text == str(WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'awsui-util-action-stripe-title'))).text):
+            time.sleep(1.5)
+            max_wait -= 1.5
+            if 0 >= max_wait:
+                print('The page loading did not finish in more than 1 minute.')
+                exit(6)
+    except TimeoutException as err:
+        if args.debug:
+            logging.exception(err)
+        print('The question text at the top of the page was not found.')
+        exit(6)
+
+def save_milestone_and_pdf(driver, configs):
+    milestone_name = configs.get('WAR', 'milestone')
+    save_milestone_button = get_element(driver, (By.ID, 'viewWorkload-recordMilestone'), 'clickable')
+    save_milestone_button.click()
+    milestone_name_inputbox = get_element(driver, (By.NAME, 'milestoneName'), 'clickable')
+    milestone_name_inputbox.send_keys()
+    save_button = get_element(driver, (By.ID, 'viewWorkloadRecordMilestoneRecordButton'), 'clickable')
+    save_button.click()
+    generate_pdf_button = get_element(driver, (By.ID, 'viewWorkload_generatePDFButton'), 'clickable')
+    generate_pdf_button.click()
+
+def review(driver, configs):
+    ignore_answers_count_mismatch = False
+    start_review_button = get_element(driver, (By.LINK_TEXT, 'Start review'), 'clickable')
+    start_review_button.click()
+    sections = configs.sections()
+    sections_count = len(sections)
+    
+    for section in sections:
+        if not section.startswith('QUESTION'):
+            continue
+        # Get the question text to compare later for loading state checking
+        question_text = str(WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, 'awsui-util-action-stripe-title'))).text)
+        if args.debug:
+            logging.info(question_text)
+            print(question_text)
+        keys = configs.options(section) 
+        # Removing default parameter 'signin.url'
+        try:
+            keys.remove('signin.url') 
+        except: 
+            pass
+        does_not_apply = False
+        notes = ''
+        if configs.has_option(section, 'doNotApply'):
+            does_not_apply = configs.getboolean(section, 'doNotApply')
+            keys.remove('donotapply')
+        if configs.has_option(section, 'notes'):
+            notes = configs.get(section, 'notes')
+            keys.remove('notes')
+
+        # 'Question does not apply to this workload' toggle button
+        toggle_button = get_element(driver, (By.CSS_SELECTOR, '*[id^="awsui-toggle"]'), 
+                                    'clickable')
+        is_checked = False
+        if str(toggle_button.get_attribute('class')).endswith('checked'):
+            is_checked = True      
+        if does_not_apply != is_checked:
+            toggle_button.click()
+
+        # Notes
+        if 0 < len(notes):
+            notes_textarea = get_element(driver, (By.CSS_SELECTOR, 'textarea[id^="awsui-textarea"][name="answerNotes"]'), 
+                                         'clickable')
+            notes_textarea.send_keys(notes)
+        if not does_not_apply:
+            # Questions section automation
+            question_checkboxes = get_elements(driver, (By.CSS_SELECTOR, 
+                                                        'input[id^="awsui-checkbox"]'), 'clickable')
+            for key in keys:
+                if not key.isdigit():
+                    if args.debug:
+                        logging.error('Expected: \'question.NUMBER.NUMBER\'\nGot: ' + key)
+                    print('Invalid answer numbering in the input file \'' + section + '\' section')
+                    print('Expected: number\nGot: ' + key)
+                    exit(4)
+                if configs.getboolean(section, key):
+                    try:
+                        checkbox = question_checkboxes[int(key) - 1]
+                        checkbox.click()
+                    except IndexError as err:
+                        if args.debug:
+                            logging.exception(err)
+                        if not ignore_answers_count_mismatch:
+                            print('Answers count mismatch for question \'' + section + '\'')
+                            print('Expected (in input file): ' + str(len(keys)))
+                            print('Actual: ' + str(len(question_checkboxes)))
+                            answer = request_data('Do you want to continue and ignore all such mismatches? (y/n)', input_mask = False)
+                            if answer.lower().startswith('y'):
+                                ignore_answers_count_mismatch = True
+                                break
+                            else:
+                                print('The script exited')
+                                exit(4)
+        # If not the last question
+        if sections.index(section) + 1 < sections_count:
+            next_button = get_element(driver, (By.ID, 'questionWizard-nextQuestionButton'), 'clickable')
+            next_button.click()
+            check_loading_state(driver, question_text)
+        # Add some randomness
+        time.sleep(random.random())
+    save_and_exit_button = get_element(driver, (By.ID, 'questionWizard-saveAndExitButton-finalQuestion'), 'clickable')
+    save_and_exit_button.click()
+    # TODO implemented partly
+    #save_milestone_and_pdf(driver, configs)
+    time.sleep(60)
 
 def run(username, password, configs):
     try:
@@ -360,30 +483,41 @@ def run(username, password, configs):
         select_region(driver, 'N. Virginia')
         open_war_service(driver)
         create_workload(driver, configs)
+        review(driver, configs)
         # TODO remove time.sleep call below
         time.sleep(60)
         driver.quit()
     except Exception as err:
-        logging.debug(err)
+        if args.debug:
+            logging.exception(err)
         print(str(err))
         exit(6)
 
 def main():
-    script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-    setup_input_args(script_dir)
-    if args.debug:
-        log_dir = os.path.join(script_dir, 'log')
-        make_directory(log_dir)
-        log_file_path = os.path.join(log_dir, 'debug.log')
-        logging_setup(log_file_path)
-    check_chrome_driver_existence()
-    configs = get_input_data(args.input_file_path)
-    username = request_data('Username')
-    password = request_data('Password')
-    current_datetime = datetime.datetime.now()
-    print("Started: " + current_datetime.strftime('%Y-%m-%d %H:%M:%S'))
-    run(username, password, configs)
-    current_datetime = datetime.datetime.now()
-    print("Ended: " + current_datetime.strftime('%Y-%m-%d %H:%M:%S'))
+    try:
+        script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+        setup_input_args(script_dir)
+        if args.debug:
+            log_dir = os.path.join(script_dir, 'log')
+            make_directory(log_dir)
+            log_file_path = os.path.join(log_dir, 'debug.log')
+            logging_setup(log_file_path)
+        check_chrome_driver_existence()
+        configs = get_input_data(args.input_file_path)
+        username = request_data('Username')
+        password = request_data('Password')
+        current_datetime = datetime.datetime.now()
+        print("Started: " + current_datetime.strftime('%Y-%m-%d %H:%M:%S'))
+        run(username, password, configs)
+        current_datetime = datetime.datetime.now()
+        print("Ended: " + current_datetime.strftime('%Y-%m-%d %H:%M:%S'))
+    except KeyboardInterrupt as err:
+        print('\nScript execution canceled by the user')
+        if args.debug:
+            logging.exception(err)
+    except Exception as err:
+        print('\n' + str(err))
+        if args.debug:
+            logging.exception(err)
 
 main()
